7371e29e0f8ffd01991d7a54e9fdf6b8fbb1a433
Seunghwan <hetzer12@icloud.com>
2020-04-28 21:47:14 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import React from 'react';
 
-function Food({ favourite }) {
+const foodILike = [
+  {
+    name: "Apple",
+    images: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/800px-Apple_logo_black.svg.png",
+  },
+  {
+    name: "Banana",
+    images: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/Banana-Single.jpg/1020px-Banana-Single.jpg",
+  },
+  {
+    name: "Grape",
+    images: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Wine_grapes03.jpg/800px-Wine_grapes03.jpg",
+  },
+  {
+    name: "Peach",
+    images: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Peach_fruit_3.jpg/1280px-Peach_fruit_3.jpg",
+  }
+];
+
+function Food({ name, picture }) {
   return (
-    <h1>I like {favourite}!</h1>
+    <div>
+      <h1>I like {name}!</h1>
+      <img src={picture} />
+    </div>
   );
 }
 
@@ -10,10 +32,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-      <Food favourite="apple" />
-      <Food favourite="banana" />
-      <Food favourite="grape" />
-      <Food favourite="peach" />
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.images} />)}
     </div>
   );
 }
